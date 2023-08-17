c0c471cfa3ecbbe4f82977fc14e7f5ecd16ac932
Divyanshi <101087149+divyanshi2000@users.noreply.github.com>
2023-08-17 23:31:33 +0530

    Update Open_account.py

--- Open_account.py
+++ Open_account.py
@@ -5,36 +5,45 @@ from selenium.webdriver.support.ui import Select
 import time
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 #import open_account
 
 driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
 driver.maximize_window()
 time.sleep(3)
 driver.find_element(By.XPATH, value='//button[text()="Bank Manager Login"]').click()
-time.sleep(2)
+
 print("Bank Manager Login successfully")
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[1]/button[2]").click()
-time.sleep(2)
+driver.find_element(By.XPATH, value="//button[contains(text(), 'Open Account')]").click()
+time.sleep(1)
 print("clicked on open account successfully")
+# Verify User Select dropdown visibility
+user_select_dropdown = driver.find_element(By.XPATH, '//select[@id="userSelect"]')
+assert user_select_dropdown.is_displayed(), "User Select dropdown is not visible."
+
 driver.find_element(By.XPATH, value='//*[@id="userSelect"]').click()
-time.sleep(2)
+
 element=driver.find_element(By.XPATH, value='//select[@id="userSelect"]')
-time.sleep(2)
 drp=Select(element)
 drp.select_by_visible_text("Neville Longbottom")
-time.sleep(2)
+time.sleep(1)
+# Verify Currency dropdown visibility
+currency_dropdown = driver.find_element(By.XPATH, '//select[@id="currency"]')
+assert currency_dropdown.is_displayed(), "Currency dropdown is not visible."
+
 driver.find_element(By.XPATH, value='//*[@id="currency"]').click()
-time.sleep(2)
 element=driver.find_element(By.XPATH, value='//select[@id="currency"]')
-time.sleep(2)
+time.sleep(1)
 drp=Select(element)
 drp.select_by_visible_text("Pound")
-time.sleep(2)
-if driver.find_element(By.XPATH,"//div//button[text()='Process']").is_displayed():
-   print("Process button is visible.")
-else:
-   print("Process button is not visible.")
+time.sleep(1)
+
+# Verify Process button visibility
+process_button = driver.find_element(By.XPATH, "//div//button[text()='Process']")
+assert process_button.is_displayed(), "Process button is not visible."
+
 driver.find_element(By.XPATH, value="//div//button[text()='Process']").click()
 print("Account created successfully with account Number :1019")
-time.sleep(5)
+
+# Close the browser
+driver.quit()
