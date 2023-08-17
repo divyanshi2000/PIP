1c4ef072b7f4fd48244f4c782c507fafe56913ff
Divyanshi <101087149+divyanshi2000@users.noreply.github.com>
2023-08-17 23:30:34 +0530

    Update Add_customer.py

--- Add_customer.py
+++ Add_customer.py
@@ -6,40 +6,42 @@ import time
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 
 #import Add_customer details
 
 driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
 driver.maximize_window()
-if driver.find_element(By.XPATH,"//div//button[text()='Home']").is_displayed():
-   print("Home button is visible.")
-else:
-   print("Home button is not visible.")
+# Assertions for Home button
+home_button = driver.find_element(By.XPATH, "//div//button[text()='Home']")
+assert home_button.is_displayed(), "Home button is not visible."
+print("Home button is visible.")
 
-if driver.find_element(By.XPATH,"//div//strong[text()='XYZ Bank']").is_displayed():
-   print("XYZ Bank is visible.")
-else:
-   print("XYZ Bank is not visible.")
-time.sleep(3)
+# Assertions for XYZ Bank
+xyz_bank = driver.find_element(By.XPATH, "//div//strong[text()='XYZ Bank']")
+assert xyz_bank.is_displayed(), "XYZ Bank is not visible."
+print("XYZ Bank is visible.")
 driver.find_element(By.XPATH, value='//button[text()="Bank Manager Login"]').click()
 print("Bank Manager Login successfully")
-time.sleep(2)
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[1]/button[1]").click()
-time.sleep(2)
+driver.implicitly_wait(10)
+
+driver.find_element(By.XPATH, value="//button[contains(text(), 'Add Customer')]").click()
+
 print("Clicked on ADD customer successfully")
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input").send_keys("Divyanshi")
-time.sleep(2)
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input").send_keys("Rajput")
-time.sleep(2)
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input").send_keys("DR0012345")
-time.sleep(2)
-if driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div/div[2]/div/div/form/button").is_displayed():
-   print("Add customer button is visible.")
-else:
-   print("Add customer button is not visible.")
+driver.implicitly_wait(10)
+driver.find_element(By.XPATH, value="//input[@type='text']").send_keys("Divyanshi")
+
+driver.find_element(By.XPATH, value="//input[@placeholder='Last Name']").send_keys("Rajput")
+
+driver.find_element(By.XPATH, value="//input[@placeholder='Post Code']").send_keys("DR0012345")
+
+# Assertions for Add Customer button
+add_customer_button = driver.find_element(By.XPATH, "//div//button[text()='Add Customer']")
+assert add_customer_button.is_displayed(), "Add Customer button is not visible."
+print("Add Customer button is visible.")
 driver.find_element(By.XPATH, value="//div//button[text()='Add Customer']").click()
 time.sleep(2)
 print("Customer added successfully with customer id")
 
-time.sleep(5)
+# Close the browser
+driver.quit()
