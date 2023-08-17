7dfbf6dd090067673fdd8e54811629647eb001c6
Divyanshi <101087149+divyanshi2000@users.noreply.github.com>
2023-08-17 23:31:03 +0530

    Update Deposit.py

--- Deposit.py
+++ Deposit.py
@@ -4,37 +4,41 @@ from selenium.webdriver.support.ui import Select
 import time
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 
 driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
 driver.maximize_window()
 time.sleep(2)
 driver.find_element(By.XPATH, value="//div//button[text()='Customer Login']").click()
 print("Customer login sucessfully")
-time.sleep(2)
+
 driver.find_element(By.XPATH, value='//*[@id="userSelect"]').click()
-time.sleep(2)
+
 element=driver.find_element(By.XPATH, value='//select[@id="userSelect"]')
 drp=Select(element)
 drp.select_by_visible_text("Hermoine Granger")
 time.sleep(2)
-if driver.find_element(By.XPATH,"//div//button[text()='Login']").is_displayed():
-   print("Login button is visible.")
-else:
-   print("Login button is not visible.")
+# Assertion for Login button visibility
+login_button = driver.find_element(By.XPATH, "//div//button[text()='Login']")
+assert login_button.is_displayed(), "Login button is not visible."
+print("Login button is visible.")
+
 driver.find_element(By.XPATH, value="//div//button[text()='Login']").click()
 print("Login sucessfully")
 time.sleep(2)
 
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[3]/button[2]").click()
-time.sleep(2)
-driver.find_element(By.XPATH, value="/html/body/div/div/div[2]/div/div[4]/div/form/div/input").send_keys("1000")
+driver.find_element(By.XPATH, value="//button[contains(text(), 'Deposit')]").click()
+
+driver.find_element(By.XPATH, value="//input[@ng-model='amount']").send_keys("1000")
 print("Amount to be Deposited")
 time.sleep(2)
 driver.find_element(By.XPATH, value="//div//button[text()='Deposit']").click()
 print("Deposit Successful")
-if driver.find_element(By.XPATH,"//div//button[text()='Logout']").is_displayed():
-   print("Logout button is visible.")
-else:
-   print("Logout button is not visible.")
-time.sleep(5)
+
+# Assertion for Logout button visibility
+logout_button = driver.find_element(By.XPATH, "//div//button[text()='Logout']")
+assert logout_button.is_displayed(), "Logout button is not visible."
+print("Logout button is visible.")
+
+# Close the browser
+driver.quit()
